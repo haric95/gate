e9ab5c3178e0ea478ba56e0440581f70969b3e31
Hari Chauhan <hari-c@hotmail.co.uk>
2022-07-16 23:23:12 +0100

    Update to propagate

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,37 @@ import {
 
 const App: React.FC = () => {
   return (
-    <Canvas orthographic>
-      <Suspense fallback={null}>
-        <Camera />
-        <color attach="background" args={[0.08, 0.08, 0.08]} />
-        <Logo />
-      </Suspense>
-      <EffectComposer>
-        <Bloom
-          luminanceThreshold={0}
-          luminanceSmoothing={0.9}
-          height={200}
-          opacity={2}
-        />
-        <Noise opacity={0.05} />
-        <Vignette eskil={false} offset={0.1} darkness={1.1} />
-        <DotScreen />
-      </EffectComposer>
-    </Canvas>
+    <>
+      <Canvas orthographic>
+        <Suspense fallback={null}>
+          <Camera />
+          <color attach="background" args={[0.08, 0.08, 0.08]} />
+          <Logo />
+        </Suspense>
+        <EffectComposer>
+          <Bloom
+            luminanceThreshold={0}
+            luminanceSmoothing={0.9}
+            height={200}
+            opacity={2}
+          />
+          <Noise opacity={0.05} />
+          <Vignette eskil={false} offset={0.1} darkness={1.1} />
+          <DotScreen opacity={0.5} />
+        </EffectComposer>
+      </Canvas>
+      <button
+        className="buy-button"
+        onClick={() =>
+          window.open(
+            "https://www.paypal.com/paypalme/gatecollective",
+            "_blank"
+          )
+        }
+      >
+        Execute Purchase
+      </button>
+    </>
   );
 };
 
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,39 +1,95 @@
 import { useGLTF } from "drei";
 import React, { useMemo, useRef, useState } from "react";
-import { useFrame } from "react-three-fiber";
-import { MeshNormalMaterial, Object3D } from "three";
+import { useFrame, useThree } from "react-three-fiber";
+import { Color, MeshBasicMaterial, MeshNormalMaterial, Object3D } from "three";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 type GLTFLogoResult = GLTF & {
   nodes: {
-    path1203: THREE.Mesh;
+    A_3_: THREE.Mesh;
   };
 };
+
+const NORMAL_SHADER = `
+#define NORMAL
+  uniform float opacity;
+
+  #if defined( FLAT_SHADED ) || defined( USE_BUMPMAP ) || defined( TANGENTSPACE_NORMALMAP )
+  	varying vec3 vViewPosition;
+  #endif
+  #ifndef FLAT_SHADED
+  	varying vec3 vNormal;
+  	#ifdef USE_TANGENT
+  		varying vec3 vTangent;
+  		varying vec3 vBitangent;
+  	#endif
+  #endif
+  #include <packing>
+  #include <uv_pars_fragment>
+  #include <bumpmap_pars_fragment>
+  #include <normalmap_pars_fragment>
+  #include <logdepthbuf_pars_fragment>
+  #include <clipping_planes_pars_fragment>
+
+  vec3 createColor( const in vec3 normal ) {
+    return normalize( (vec3(0.0, 1.0, 0.05) * normal.z - 0.5) + vec3(0.0, 1.0, 0.05) * 0.2 );
+  }
+
+  void main() {
+  	#include <clipping_planes_fragment>
+  	#include <logdepthbuf_fragment>
+  	#include <normal_fragment_begin>
+  	#include <normal_fragment_maps>
+  	gl_FragColor = vec4( createColor( normal ) , opacity );
+  }
+`;
 
 export const Logo: React.FC = () => {
   const logoRef = useRef<null | Object3D>(null);
 
   const { nodes: logoNodes } = useGLTF(
-    "/gate.glb"
+    "/propagate.glb"
   ) as unknown as GLTFLogoResult;
 
   const [isMouseOver, setIsMouseOver] = useState(false);
+  const [isWireframe, setIsWireframe] = useState(false);
+
+  const { viewport } = useThree();
 
   useFrame(() => {
     if (logoRef.current) {
       logoRef.current.rotateZ(0.01);
-      if (isMouseOver) {
-        logoRef.current.rotateY(0.005);
+      const rand = Math.random();
+
+      if (isWireframe) {
+        if (rand < 0.1) {
+          setIsWireframe(false);
+        }
+      } else {
+        if (rand < 0.05) {
+          setIsWireframe(true);
+        }
       }
     }
   });
 
   const regMaterial = useMemo(() => {
-    return new MeshNormalMaterial({ wireframe: false });
+    const material = new MeshNormalMaterial({ wireframe: false });
+
+    material.onBeforeCompile = (shader) => {
+      shader.fragmentShader = NORMAL_SHADER;
+    };
+
+    return material;
   }, []);
 
   const wireframeMaterial = useMemo(() => {
-    return new MeshNormalMaterial({ wireframe: true });
+    const material = new MeshBasicMaterial({
+      wireframe: true,
+      color: new Color("rgb(0, 255, 30)"),
+    });
+
+    return material;
   }, []);
 
   return (
@@ -42,11 +98,11 @@ export const Logo: React.FC = () => {
         onPointerOver={() => setIsMouseOver(true)}
         onPointerOut={() => setIsMouseOver(false)}
         ref={logoRef}
-        material={isMouseOver ? wireframeMaterial : regMaterial}
-        geometry={logoNodes.path1203.geometry}
-        scale={[5000, 5000, 5000]}
-        position={[0, 0, 0]}
-        rotation={[Math.PI / 2.5, 0, 0]}
+        material={isWireframe ? wireframeMaterial : regMaterial}
+        geometry={logoNodes.A_3_.geometry}
+        scale={viewport.width > 640 ? [3000, 3000, 3000] : [1500, 1500, 1500]}
+        position={[0, 50, 0]}
+        rotation={[Math.PI / 2.6, 0, 0]}
       ></mesh>
     </>
   );
