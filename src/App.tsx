53621c148c4cd9a5058471f249051bba5d5ee698
Hari Chauhan <hari-c@hotmail.co.uk>
2023-05-31 08:46:04 +0100

    add password

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,19 @@ import React, { useCallback, useEffect, useState } from "react";
 
 const COLORS = ["#ffffff", "#a1d182", "#6dba2d", "#11af00"];
 
+const PASSWORD = "grabaticketeverybody";
+
 const App: React.FC = () => {
   const [isSlim, setIsSlim] = useState(false);
+  const [hasEnteredPassword, setHasEnteredPassword] = useState(false);
+  const [passwordValue, setPasswordValue] = useState("");
 
-  const keydownHandler = useCallback<EventListener>((e) => {
-    if ((e as KeyboardEvent).keyCode === 83) {
-      setIsSlim(true);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordValue(e.target.value);
+    if (e.target.value === PASSWORD) {
+      setHasEnteredPassword(true);
     }
-  }, []);
-  const keyupHandler = useCallback<EventListener>((e) => {
-    if ((e as KeyboardEvent).keyCode === 83) {
-      setIsSlim(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("keydown", keydownHandler, false);
-    window.addEventListener("keyup", keyupHandler, false);
-    return () => {
-      window.removeEventListener("keydown", keydownHandler, false);
-      window.removeEventListener("keyup", keyupHandler, false);
-    };
-  }, [keydownHandler, keyupHandler]);
+  };
 
   const [[rand1, rand2], _] = useState(() => {
     return [
@@ -42,7 +33,7 @@ const App: React.FC = () => {
           padding: 24,
         }}
       >
-        <div>
+        <div className="left">
           <div style={{ zIndex: 1, marginBottom: 24 }}>
             <h2 style={{ color: COLORS[0] }}>Grab</h2>
             <h2 style={{ color: COLORS[1] }}>A</h2>
@@ -96,11 +87,11 @@ const App: React.FC = () => {
               }}
             >
               Please send the payment as{" "}
-              <span style={{ color: COLORS[2], textDecoration: "underline" }}>
+              <span style={{ color: COLORS[1], textDecoration: "underline" }}>
                 ‘friends and family’
               </span>{" "}
               + include your{" "}
-              <span style={{ color: COLORS[2], textDecoration: "underline" }}>
+              <span style={{ color: COLORS[1], textDecoration: "underline" }}>
                 email address
               </span>{" "}
               with the payment
@@ -114,42 +105,59 @@ const App: React.FC = () => {
               }}
             >
               Ticket includes{" "}
-              <span style={{ color: COLORS[3] }}>
+              <span style={{ color: COLORS[2] }}>
                 camping on August 4th & 5th
               </span>{" "}
-              + <span style={{ color: COLORS[3] }}>home cooked meals</span>
+              + <span style={{ color: COLORS[2] }}>home cooked meals</span>
             </p>
-            <button
-              className="buy-button"
-              onMouseEnter={() => {
-                setIsSlim(true);
-              }}
-              onMouseLeave={() => {
-                setIsSlim(false);
-              }}
-              onClick={() =>
-                window.open(
-                  "https://www.paypal.com/paypalme/gatecollective/75",
-                  "_blank"
-                )
-              }
-              style={{
-                padding: 8,
-                color: "black",
-                border: "2px solid #26256B",
-                cursor: "pointer",
-                marginBottom: 16,
-                fontSize: 16,
-              }}
-            >
-              Execute Purchase
-            </button>
+            {hasEnteredPassword ? (
+              <button
+                className="buy-button"
+                onMouseEnter={() => {
+                  setIsSlim(true);
+                }}
+                onMouseLeave={() => {
+                  setIsSlim(false);
+                }}
+                onClick={() =>
+                  window.open(
+                    "https://www.paypal.com/paypalme/gatecollective/75",
+                    "_blank"
+                  )
+                }
+                style={{
+                  padding: 8,
+                  color: "black",
+                  border: "2px solid #26256B",
+                  cursor: "pointer",
+                  marginBottom: 16,
+                  fontSize: 16,
+                }}
+              >
+                Execute Purchase
+              </button>
+            ) : (
+              <div style={{ color: COLORS[0], marginTop: 24 }}>
+                <p style={{ marginBottom: 8, color: COLORS[3] }}>
+                  Enter password to continue with purchase.
+                </p>
+                <div>
+                  <p style={{ fontSize: 12 }}>Password</p>
+                  <input
+                    type="password"
+                    className="password-box"
+                    value={passwordValue}
+                    onChange={handleChange}
+                  />
+                  <button>Submit</button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div
           className="right"
           style={{
-            width: "100%",
             position: "relative",
           }}
         >
